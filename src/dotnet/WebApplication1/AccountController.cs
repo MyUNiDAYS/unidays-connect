1eeeb4bd5033af5814c829e31f0afae58e33d0ab
Andrew Bullock <trullock@gmail.com>
2019-07-02 21:01:41 +0100

    initial import

---
+++ src/dotnet/WebApplication1/AccountController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1
+{
+	[Route("[controller]/[action]")]
+	public class AccountController : Controller
+	{
+		[HttpGet]
+		public IActionResult Login(string returnUrl = "/")
+		{
+			return Challenge(new AuthenticationProperties() { RedirectUri = returnUrl });
+		}
+	}
+}
